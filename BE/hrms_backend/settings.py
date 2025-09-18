1f7bf202d58278d526d89fb7be524bc49625e3ae
digitalinnovatorstech <digital@innovatorstech.com>
2025-09-18 09:54:58 +0530

    Update settings.py
    Updated database endpoint and usernames

--- BE/hrms_backend/settings.py
+++ BE/hrms_backend/settings.py
@@ -208,10 +208,10 @@ WSGI_APPLICATION = 'hrms_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'newdb',
-        'USER': 'postgres',
-        'PASSWORD': '12345',
-        'HOST': 'localhost',
+        'NAME': 'hrms',
+        'USER': 'Hrms',
+        'PASSWORD': 'Hrms12345678',
+        'HOST': 'hrms-db.cxmagy8eyrol.ap-south-2.rds.amazonaws.com',
         'PORT': '5432',
     }
 }
